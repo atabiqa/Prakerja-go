2f1755d0f477cac1e9933e7d165b41a1b1ca03b4
atabiq <139938511+atabiqa@users.noreply.github.com>
2023-10-06 07:58:46 +0700

    Update database.go

--- configs/database.go
+++ configs/database.go
@@ -8,8 +8,7 @@ import (
 var DB *gorm.DB
 
 func InitDatabase() {
-	dsn := "root:zahrotun@tcp(127.0.0.1:3306)/prakerja11?charset=utf8mb4&parseTime=True&loc=Local"
-	var err error
+	
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 
